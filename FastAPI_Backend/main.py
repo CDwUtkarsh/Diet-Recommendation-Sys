7b9ff20ab93edfcb0a3e9a0b0f482379d843b2f0
Utkarsh Tiwari <160241457+CDwUtkarsh@users.noreply.github.com>
2026-03-07 14:46:22 +0530

    Update main.py

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from model import recommend,output_recommended_recipes
 
 
-dataset=pd.read_csv('../Data/dataset.csv',compression='gzip')
+self.dataset = pd.read_csv("Data/dataset.csv", encoding="latin1")
 
 app = FastAPI()
 
